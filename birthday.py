41612d242bbeb3337d5679207ee4da928e3c79eb
Eric Dennison <ericd@netdenizen.com>
2015-09-14 20:50:40 -0400

    Create birthday.py

---
+++ birthday.py
@@ -0,0 +1,27 @@
+Your program will ask the user the following questions, in this order:
+
+Their name.
+The name of the month they were born in (e.g. "September").
+The year they were born in (e.g. "1962").
+The day they were born on (e.g. "11").
+If the user's birthday fell on October 31, then respond with:
+
+You were born on Halloween!
+
+If the user's birthday fell on today's date, then respond with:
+
+Happy birthday!
+
+Otherwise respond with a statement like this:
+
+Peter, you are a winter baby of the nineties.
+
+Example Session
+
+So a typical session would look like this (remember: details matter!)
+
+Hello, what is your name? Eric
+Hi Eric, what was the name of the month you were born in? September
+And what year were you born in, Eric? 1972
+And the day? 11
+Eric, you are a fall baby of the stone age.
